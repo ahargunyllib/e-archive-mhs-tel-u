5eeda9498db8d284eac8cb47fb1f07f34cfba04e
ahargunyllib <billy.bpm03@gmail.com>
2025-06-12 13:10:00 +0700

    fix(table): enhance table styling with improved border colors and text formatting

--- resources/js/shared/components/ui/table.tsx
+++ resources/js/shared/components/ui/table.tsx
@@ -21,7 +21,10 @@ function TableHeader({ className, ...props }: React.ComponentProps<"thead">) {
 	return (
 		<thead
 			data-slot="table-header"
-			className={cn("[&_tr]:border-b", className)}
+			className={cn(
+				"[&_tr]:border-b [&_tr]:border-[#EAECF0] bg-[#F9FAFB]",
+				className,
+			)}
 			{...props}
 		/>
 	);
@@ -42,7 +45,7 @@ function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
 		<tfoot
 			data-slot="table-footer"
 			className={cn(
-				"bg-muted/50 border-t font-medium [&>tr]:last:border-b-0",
+				"bg-muted/50 border-t border-[#EAECF0] font-medium [&>tr]:last:border-b-0",
 				className,
 			)}
 			{...props}
@@ -55,7 +58,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
 		<tr
 			data-slot="table-row"
 			className={cn(
-				"hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+				"hover:bg-muted/50 data-[state=selected]:bg-muted border-b border-b-[#EAECF0] transition-colors",
 				className,
 			)}
 			{...props}
@@ -68,7 +71,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
 		<th
 			data-slot="table-head"
 			className={cn(
-				"text-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+				"text-[#667085] font-normal text-xs px-4 py-3 text-left align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
 				className,
 			)}
 			{...props}
@@ -81,7 +84,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
 		<td
 			data-slot="table-cell"
 			className={cn(
-				"p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+				"text-[#090E17] text-xs px-4 py-3 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
 				className,
 			)}
 			{...props}
