14eab8c0090434701a2d691176656b75aaa031a5
ahargunyllib <billy.bpm03@gmail.com>
2025-06-17 17:07:48 +0700

    feat(layout): implement RootLayout component and useFlashMessages hook for flash message handling

--- resources/js/shared/components/layouts/dashboard-layout.tsx
+++ resources/js/shared/components/layouts/dashboard-layout.tsx
@@ -21,6 +21,7 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import RootLayout from "./root-layout";
 
 const navItems = [
 	{ name: "Dashboard", icon: HomeIcon, href: "/dashboard/home" },
@@ -53,80 +54,82 @@ export default function DashboardLayout({
 	const { user } = useAuth();
 
 	return (
-		<section className="min-h-dvh relative">
-			<nav className="w-full flex items-center justify-between px-20 py-4">
+		<RootLayout>
+			<section className="min-h-dvh relative">
+				<nav className="w-full flex items-center justify-between px-20 py-4">
+					<img
+						src="/logo.png"
+						alt="Logo"
+						className="relative h-12 object-contain"
+					/>
+					<div className="flex items-center bg-[#F2F4F7] rounded-full h-13 p-2">
+						{navItems.map((item) => (
+							<Link
+								key={item.name}
+								href={item.href}
+								className={cn(
+									"flex items-center gap-x-2 p-3",
+									url.startsWith(item.href) &&
+										"bg-[#090E17] text-[#F9FAFB] rounded-full",
+								)}
+							>
+								<item.icon className="size-4" />
+								<span className="font-medium text-xs">{item.name}</span>
+							</Link>
+						))}
+					</div>
+
+					<div className="flex items-center gap-x-1">
+						<Button className="bg-[#F2F4F7] hover:bg-[#F2F4F7]/80 text-[#101828] rounded-full aspect-square h-13">
+							<BellIcon // TODO
+								className="size-4"
+							/>
+						</Button>
+
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button className="bg-[#F2F4F7] hover:bg-[#F2F4F7]/80 text-[#101828] rounded-full h-13">
+									<Avatar>
+										<AvatarImage
+											src={user.photo_profile ?? undefined} // TODO
+											alt="Avatar"
+										/>
+										<AvatarFallback>{user.name[0]}</AvatarFallback>
+									</Avatar>
+									<ChevronDownIcon className="size-4" />
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuItem>
+									<Link
+										href="/dashboard/profile"
+										className="w-full flex items-center gap-x-2"
+									>
+										<UserIcon /> Profil
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<Link
+										href="/dashboard/settings" // TODO
+										className="w-full flex items-center gap-x-2"
+									>
+										<LogOutIcon /> Logout
+									</Link>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</div>
+				</nav>
+
+				<div className={cn("py-10 px-20 space-y-6", className)}>{children}</div>
+
+				<div className="absolute -z-20 bg-[#F9FAFB] w-full h-full bottom-0" />
 				<img
-					src="/logo.png"
-					alt="Logo"
-					className="relative h-12 object-contain"
+					src="/background.png"
+					alt="Background"
+					className="absolute inset-0 -z-10 object-cover w-full h-64"
 				/>
-				<div className="flex items-center bg-[#F2F4F7] rounded-full h-13 p-2">
-					{navItems.map((item) => (
-						<Link
-							key={item.name}
-							href={item.href}
-							className={cn(
-								"flex items-center gap-x-2 p-3",
-								url.startsWith(item.href) &&
-									"bg-[#090E17] text-[#F9FAFB] rounded-full",
-							)}
-						>
-							<item.icon className="size-4" />
-							<span className="font-medium text-xs">{item.name}</span>
-						</Link>
-					))}
-				</div>
-
-				<div className="flex items-center gap-x-1">
-					<Button className="bg-[#F2F4F7] hover:bg-[#F2F4F7]/80 text-[#101828] rounded-full aspect-square h-13">
-						<BellIcon // TODO
-							className="size-4"
-						/>
-					</Button>
-
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button className="bg-[#F2F4F7] hover:bg-[#F2F4F7]/80 text-[#101828] rounded-full h-13">
-								<Avatar>
-									<AvatarImage
-										src={user.photo_profile ?? undefined} // TODO
-										alt="Avatar"
-									/>
-									<AvatarFallback>{user.name[0]}</AvatarFallback>
-								</Avatar>
-								<ChevronDownIcon className="size-4" />
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end">
-							<DropdownMenuItem>
-								<Link
-									href="/dashboard/profile"
-									className="w-full flex items-center gap-x-2"
-								>
-									<UserIcon /> Profil
-								</Link>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<Link
-									href="/dashboard/settings" // TODO
-									className="w-full flex items-center gap-x-2"
-								>
-									<LogOutIcon /> Logout
-								</Link>
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				</div>
-			</nav>
-
-			<div className={cn("py-10 px-20 space-y-6", className)}>{children}</div>
-
-			<div className="absolute -z-20 bg-[#F9FAFB] w-full h-full bottom-0" />
-			<img
-				src="/background.png"
-				alt="Background"
-				className="absolute inset-0 -z-10 object-cover w-full h-64"
-			/>
-		</section>
+			</section>
+		</RootLayout>
 	);
 }
