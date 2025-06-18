a94b123fdb2b3470837585bce7f2eb0fe40b39c0
ahargunyllib <billy.bpm03@gmail.com>
2025-06-18 17:20:06 +0700

    feat(achievements): create achievements table with necessary fields and defaults

---
+++ database/migrations/2025_06_18_100749_create_achievements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name');
+            $table->date('date');
+            $table->integer("type")->default(1); // 1: internal, 2: external
+            $table->integer("set_type")->default(1); // 1: regular, 2: special
+            $table->string('certificate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achievements');
+    }
+};
