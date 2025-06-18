8d4f81510411e54f30a7e0a1180cc4e1fae4b942
ahargunyllib <billy.bpm03@gmail.com>
2025-06-18 18:37:44 +0700

    feat(routes): update home route to use AuthenticatedSessionController

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,11 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AchievementController;
 use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', function () {
